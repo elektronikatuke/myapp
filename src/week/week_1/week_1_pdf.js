cd6e3389b8fb1ca62542d03192845237207bffd3
elektronikatuke.github.io <101970610+elektronikatuke@users.noreply.github.com>
2022-04-19 11:16:25 +0200

    Update week_1_pdf.js

--- src/week/week_1/week_1_pdf.js
+++ src/week/week_1/week_1_pdf.js
@@ -4,7 +4,7 @@ import Pdf_view from '../../komponenty/Pdf_view';
 
 function Week1pdf(){
 
-  return <Pdf_view document="/Prednasky_1_diody.pdf" />
+  return <Pdf_view document="../../public/Prednasky_1_diody.pdf" />
 }
 
 export default Week1pdf; 
