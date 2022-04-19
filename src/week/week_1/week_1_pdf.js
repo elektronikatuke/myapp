f3f4637932d1f2d0a377b6f18afa98ec6489379f
mt648sh <martin.thern@student.tuke.sk>
2022-04-19 11:29:37 +0200

    vsetko

--- src/week/week_1/week_1_pdf.js
+++ src/week/week_1/week_1_pdf.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Pdf_view from '../../komponenty/Pdf_view';
+import { Link } from 'react-router-dom';
 
 
 function Week1pdf(){
 
-  return <Pdf_view document="/Prednasky_1_diody.pdf" />
+  return <Pdf_view document={<Link to="/Prednasky_1_diody.pdf"></Link>}/>
 }
 
 export default Week1pdf; 
